4c54ea37eaa44b375d85f5a1ce95c2b290f99afd
krhernandezf-dotco <kr.hernandezf@duocuc.cl>
2026-05-26 21:08:52 -0400

    Add files via upload

---
+++ ecocampings.py
@@ -0,0 +1,13 @@
+print("gestion de eco-Camping 'bosque vivo'")
+capacidad_maxima = 15 
+sitios_ocupados = 0 
+ejecutando = true 
+while: ejecutando 
+print("\n----- MENU DE CONTROL DE REGISTROS")
+print ("1.- VER SITIOS DISPONIBLES")
+print("2.- registro de nuevos vehiculos en el sitio (entrada)")
+prin()"3.- registo de nuevos vehiculos (salida")
+print("4.- estado acxtual del camping")
+print("5.-salir")
+
+
